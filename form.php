a18089766adbb55800d2759437cd6101da0ef487
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-04-05 01:41:36 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,80 +1,101 @@
 <?php
+// Отправляем браузеру правильную кодировку,
+// файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
+
+// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
+// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-// В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-    if (!empty($_GET['save'])) {// Если есть параметр save, то выводим сообщение пользователю.
-        print('Спасибо, результаты сохранены.');
-    }
-    include('form.php');
-    exit();
+  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
+  if (!empty($_GET['save'])) {
+    // Если есть параметр save, то выводим сообщение пользователю.
+    print('Спасибо, результаты сохранены.');
+  }
+  // Включаем содержимое файла form.php.
+  include('index.php');
+  // Завершаем работу скрипта.
+  exit();
 }
+// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
+
 // Проверяем ошибки.
 $errors = FALSE;
 if (empty($_POST['name'])) {
-    print('Введите имя<br>');
-    $errors = TRUE;
+  print('Заполните имя.<br/>');
+  $errors = TRUE;
 }
+
 if (empty($_POST['email']) or !(strpos($_POST['email'], '@'))) {
-    print('Введите e-mail<br>');
-    $errors = TRUE;
+  print('Введите e-mail.<br/>');
+  $errors = TRUE;
 }
+
 if (empty($_POST['year'])) {
-    print('Выберите из списка год рождения<br>');
-    $errors = TRUE;
+  print('Выберите год рождения.<br/>');
+  $errors = TRUE;
 }
+
 if (empty($_POST['gender'])) {
-    print('Укажите ваш пол<br>');
-    $errors = TRUE;
+  print('Укажите ваш пол.<br/>');
+  $errors = TRUE;
 }
+
+
 if (empty($_POST['kon'])){
-    print ('Выберите количество конечностей<br>');
+    print ('Выберите количество конечностей.<br>');
     $errors = true;
+
 }
+
 if (empty($_POST['super'])){
-    print ('Выберите минимум одну сверхспособность<br>');
+    print ('Выберите одну или несколько сверхспособностей.<br>');
     $errors = true;
+
 }
 else {
-    $super = serialize($_POST['super']);
+  $super = serialize($_POST['super']);
 }
+
 if (empty($_POST['bio'])){
-    print ('Расскажите о себе<br>');
+    print ('Расскажите о себе.<br>');
     $errors = true;
 }
+
 if (empty($_POST['contr_check'])){
-    print ('Обязательно ознакомьтесь с контрактом перед отправкой формы<br>');
+    print ('Вы не можете отправить форму, не ознакомившись с контрактом.<br>');
     $errors = true;
 }
-if ($errors) {// При наличии ошибок завершаем работу скрипта.
-    exit();
+
+if ($errors) {
+  // При наличии ошибок завершаем работу скрипта.
+  exit();
 }
+
 // Сохранение в базу данных.
+
 $user = 'u52925';
 $pass = '3596996';
 $db = new PDO('mysql:host=localhost;dbname=u52925', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+
 // Подготовленный запрос. Не именованные метки.
+
 try {
-    $stmt = $db->prepare("INSERT INTO person SET name = ?, email = ?, year = ?, gender = ?, limbs = ?, biography = ?");
-    $stmt -> execute(array(
-        $_POST['name'],
+  $stmt = $db->prepare("INSERT INTO person SET name = ?, email = ?, year = ?, gender = ?, limbs = ?, biography = ?");
+  $stmt -> execute(array(
+		$_POST['name'],
         $_POST['email'],
         $_POST['year'],
         $_POST['gender'],
         $_POST['kon'],
         $_POST['bio'],
-    ));
-    foreach ($_POST['super'] as $value) {
-        $stmt = $db->prepare("INSERT INTO superpower SET name = ?, superpower = ?");
-        $stmt -> execute(array(
-            $_POST['name'], 
-            $value,
-        ));
-    }
-    unset($value);
+	));
+	
+  $stmt = $db->prepare("INSERT INTO superpower SET name = ?");
+  $stmt -> execute(array(
+		$_POST['super'] = implode(', ', $_POST['super']),
+	));
 }
 catch(PDOException $e){
-print('Error: ' . $e->getMessage());
-exit();
+  print('Error: ' . $e->getMessage());
+  exit();
 }
-// stmt - это "дескриптор состояния"
-header('Location: ?save=1');
