e852da7e5440c8ced4d2a8754dc98274377c0688
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-04-04 19:58:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,75 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	<title>Task-3</title>
+    <meta charset="utf-8">
+    <title>Cверхспособности</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-	<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm">
+	<div class="forma">
+    <header>
+        <div id="название">
+            <h2>Cверхспособности</h2>
+        </div>
+    </header>
+        <form action="form.php" method="POST">
+            <label>
+                <div class="names">Введите имя:</div>
+                <input name="name" placeholder="Введите имя" />
+            </label>
+            <label>
+				<div class="names">Адрес электронной почты:</div>
+                <input name="email" type="email" placeholder="Введите email" /><br>
+            </label>
+            <label for="year" class="names">Год рождения</label>
+            <select name="year">
+     <!-- <?php 
+    for ($i = 1922; $i <= 2022; $i++) {
+      printf('<option value="%d">%d</option>', $i, $i);
+    }
+    ?> -->
+  </select>
+            <br>
+            <div class="names">Выберите пол:</div>
+            <label><input type="radio" checked="checked" name="gender" value="female" />
+                Женский</label>
+            <label><input type="radio" name="gender" value="male" />
+                Мужской</label>
+            <br>
+            <div class="names">Количество конечностей:</div>
+            <label><input type="radio" checked="checked" name="kon" value="1" />
+                1</label>
+            <label><input type="radio" name="kon" value="2" />
+                2</label>
+            <label><input type="radio" name="kon" value="3" />
+                3</label>
+            <label><input type="radio" name="kon" value="4" />
+                4</label>
+            <label>
+                <br>
+                <div class="names">Сверхспособности:</div>
+                <select name="super[]" multiple="multiple">
+                    <option value="deathless">Бессмертие</option>
+                    <option value="walls" selected="selected">Прохождение сквозь стены</option>
+                    <option value="levitation">Левитация</option>
+                    <option value="timestop">Остановка времени</option>
+                    <option value="tp">Телепортация</option>
+                </select>
+                <br>
+				<div class="names">
+                Биография: </div>
+                <textarea name="bio">Напишите о себе</textarea><br>
+                <label><input type="checkbox" checked="checked" name="contr_check" />
+                    с контрактом ознакомлен(а)</label>
+                <br>
+                <input type="submit" class="submit" value="Отправить" />
+
+        </form>
+    </div>
 	</div>
-	<div class="container">
-		<form action="form.php" method="POST">
-			<p><label for="name">Имя</label>
-			<input type="text" id="name" name="name"></p>
-			
-			<p><label for="email">E-mail</label>
-			<input type="text" id="email" name="email"></p>
-			
-			<p><label for="year">Год рождения</label>
-			<select id="year" name="year">
-				<option selected>Год</option>
-    <?php
-        for ($i = 1980; $i <= 2014; $i++)
-            echo '<option>' . $i . '</option>';
-    ?>
-			</select>
-			
-			<p><label>Пол:</label>
-			<input type="radio" id="male" value="male" name="gender">Мужской
-			<input type="radio" id="female" value="female" name="gender">Женский</p>
-			
-			<p><label>Количество конечностей:</label>
-			<input type="radio" id="0" value="0" name="kon">0
-			<input type="radio" id="1" value="1" name="kon">1
-			<input type="radio" id="2" value="2" name="kon">2
-			<input type="radio" id="3" value="3" name="kon">3
-			<input type="radio" id="4" value="4" name="kon">4</p>
-			
-			<p><label>Сверхспособности:</label>
-			<input type="checkbox" id="immortality" value="immortality" name="super[]">Бессмертие
-			<input type="checkbox" id="stena" value="stena" name="super[]">Прохождение сквозь стены
-			<input type="checkbox" id="levitation" value="levitation" name="super[]">Левитация</p>
-			
-			<p><label for="bio">Биография</label>
-			<textarea id="bio" name="bio"></textarea></p>
-			
-			<p><label>С контрактом ознакомлен:</label>
-			<input type="checkbox" id="contr_check" value="contr_check" name="contr_check"></p>
-			
-			<p><button type="submit" value="send">Отправить</p>
-		</form>
-	</div>
+
 </body>
+
 </html>
