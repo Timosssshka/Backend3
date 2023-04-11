eef7f37130f0e5f3467530a3880372e513e061ec
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2023-04-12 01:55:08 +0300

    Update form.php

--- form.php
+++ form.php
@@ -59,29 +59,20 @@ if (empty($_POST['contr_check'])){
 if ($errors) {
   exit();
 }
-$name = $_POST['name'];
-$email = $_POST['email'];
-$birth_date = $_POST['year'];
-$gender = $_POST['gender'];
-$limbs = $_POST['kon'];
-$superpowers = $_POST['super'];
-$bio = $_POST['bio'];
-$contract = $_POST['contr_check'];
 $user = 'u52925';
 $pass = '3596996';
-$conn = new PDO('mysql:host=localhost;dbname=u52925', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$db = new PDO('mysql:host=localhost;dbname=52925', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
 try {
-   $stmt = $conn->prepare("INSERT INTO person(name, email, year , gender, limbs, biography , contract)
-    VALUES (?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssiss", $name, $email, $birth_date, $gender, $limbs, $bio, $contract);
-    $stmt->execute();
-    $last_id = mysqli_insert_id($conn);
-    foreach ($superpowers as $item) {
-        $query = "INSERT INTO person_ability  (person_id , ability_id ) VALUES ('$last_id', '$item')";
-    }
-    $stmt->close();
-    $conn->close();
-}
+    $stmt = $db->prepare("INSERT INTO person SET name = ?, email = ?, year = ?, gender = ?, limbs = ?, biography = ?, contract = ?");
+    $stmt -> execute(array(
+        $_POST['name'],
+        $_POST['email'],
+        $_POST['year'],
+        $_POST['gender'],
+        $_POST['limbs'],
+        $_POST['biography'],
+	 $_POST['contr_check'],
+    ));
 	
 //  foreach ($_POST['superpowers'] as $value) {
 //         $stmt = $db->prepare("INSERT INTO superpower SET name = ?, superpower = ?");
